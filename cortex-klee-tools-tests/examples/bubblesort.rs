279c2abf1e0fa4840f6f821a43511b1359ef885a
CarlOsterberg <carl.vilhelms.osterberg@gmail.com>
2022-05-16 13:31:34 +0200

    shits fukked yo

--- cortex-klee-tools-tests/examples/bubblesort.rs
+++ cortex-klee-tools-tests/examples/bubblesort.rs
@@ -5,6 +5,7 @@
 
 use klee_sys::klee_make_symbolic;
 use panic_klee as _;
+use cortex_test_lib::bubble_sort;
 
 #[inline(never)]
 #[no_mangle]
@@ -12,22 +13,4 @@ fn main() {
     let mut vec = [3,1,4,5];
     klee_make_symbolic!(&mut vec, "vec");
     bubble_sort(&mut vec);
-}
-
-#[inline(never)]
-pub fn bubble_sort(vec: &mut [i32]) {
-    loop {
-        let mut done = true;
-        for i in 0..vec.len()-1 {
-            if vec[i+1] < vec[i] {
-                done = false;
-                let temp = vec[i+1];
-                vec[i+1] = vec[i];
-                vec[i] = temp;
-            }
-        }
-        if done {
-            return;
-        }
-    }
 }
